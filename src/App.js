3138f7233f47ce5e125f7f5fef24ed3911650ab8
Reece <83136963+Glamoore@users.noreply.github.com>
2022-07-07 19:18:54 +0100

    Fixed global context.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import {RatingProvider} from "./context/rating.context";
 import './App.css';
 
+//Pages
 import RatingApp from '../src/pages/RatingApp';
-
-
-
-
-
+import RatingThankYou from "./pages/RatingThankYou";
 
 function App() {
   return (
-    <div className="App">
-      <RatingApp />
-    </div>
+    <Router>
+      <RatingProvider>
+        <Routes>
+          <Route path="/" element={<RatingApp />} />
+          <Route path="ThankYou" element={<RatingThankYou />} />
+        </Routes>
+      </RatingProvider>
+  </Router>
   );
 }
 
